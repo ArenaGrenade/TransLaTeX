28d226de527976c99ff097a71402de30a0a56c15
Rohan Asokan <rohan.asokan@students.iiit.ac.in>
2020-12-21 18:32:15 +0400

    Added github links

--- web/src/components/Banner.jsx
+++ web/src/components/Banner.jsx
@@ -4,9 +4,10 @@ const Banner = () => {
     return (
         <Container
             fluid
-            className="text-center banner-container d-flex justify-content-center align-items-center"
+            className="text-center banner-container d-flex flex-column justify-content-center align-items-center"
         >
             <img src="/banner.svg" className="banner-img" alt="TransLaTeX" />
+            <span classname="repo-link">Hello there! Please visit us on <a href="https://github.com/ArenaGrenade/TransLaTeX">github</a>!</span>
         </Container>
     );
 };
