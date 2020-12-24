777fd35ffd2ce9174b3898a5bca40644a7bfa6c8
v15hv4 <vishva2912@gmail.com>
2020-12-24 19:57:54 +0530

    Add powered by and github badge

--- web/src/components/Banner.jsx
+++ web/src/components/Banner.jsx
@@ -4,10 +4,18 @@ const Banner = () => {
     return (
         <Container
             fluid
-            className="text-center banner-container d-flex flex-column justify-content-center align-items-center"
+            className="text-center mt-5 pt-3 mb-4 d-flex flex-column justify-content-center align-items-center"
         >
-            <img src="/banner.svg" className="banner-img" alt="TransLaTeX" />
-            <span classname="repo-link">Hello there! Please visit us on <a href="https://github.com/ArenaGrenade/TransLaTeX">github</a>!</span>
+            <div>
+                <img src="/banner.svg" className="banner-img" alt="TransLaTeX" />
+                <div className="poweredby-text text-right">
+                    Powered by
+                    <img src="/openai.svg" className="poweredby-img mx-2" alt="OpenAI" />·
+                    <a href="https://github.com/ArenaGrenade/TransLaTeX">
+                        <img src="/github.svg" className="mb-1 github-img" alt="GitHub" />
+                    </a>
+                </div>
+            </div>
         </Container>
     );
 };
